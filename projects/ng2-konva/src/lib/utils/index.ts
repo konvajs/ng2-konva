db299096204c08112e99c57af89cd559f0540431
Felix <felixoehler@yahoo.de>
2023-07-08 22:09:25 +0200

    refactor: remove unnecessary non-capturing group in regex

--- projects/ng2-konva/src/lib/utils/index.ts
+++ projects/ng2-konva/src/lib/utils/index.ts
@@ -6,7 +6,7 @@ import { ListenerRecord } from './types';
 
 function camelize(str: string): string {
   return str
-    .replace(/(?:^\w|[A-Z]|\b\w)/g, function (letter, index) {
+    .replace(/^\w|[A-Z]|\b\w/g, function (letter, index) {
       return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
     })
     .replace(/\s+/g, '');
